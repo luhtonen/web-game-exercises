8d6a1670a42f3edc0504a1add2491aa4ae2729e2
Eduard Luhtonen <luhtonen@gmail.com>
2013-11-27 17:08:29 +0100

    accessing individual sprites

--- fighting/main.js
+++ fighting/main.js
@@ -2,14 +2,27 @@ var gamejs = require('gamejs');
 var screenWidth = 1200;
 var screenHeight = 600;
 var scale = 8;
-var spriteSheetWidth = 64;
-var spriteSheetHeight = 16;
+var spriteSize = 16;
+var numSprites = 4;
 
+function scaleUp(image) {
+	return gamejs.transform.scale(image, [spriteSize*scale, spriteSize*scale]);
+}
 function main() {
 	var display = gamejs.display.setMode([screenWidth, screenHeight]);
 	var sprites = gamejs.image.load('sprites.png');
-	sprites = gamejs.transform.scale(sprites, [spriteSheetWidth*scale, spriteSheetHeight*scale]);
-	display.blit(sprites);
+	var surfaceCache = [];
+	for (var i = 0; i < numSprites; i++) {
+		var surface = new gamejs.Surface([spriteSize, spriteSize]);
+		var rect = new gamejs.Rect(spriteSize*i, 0, spriteSize, spriteSize);
+		var imgSize = new gamejs.Rect(0, 0, spriteSize, spriteSize);
+		surface.blit(sprites, imgSize, rect);
+		surfaceCache.push(surface);
+	};
+	for (var i = 0; i < surfaceCache.length; i++) {
+		var sprite = scaleUp(surfaceCache[i]);
+		display.blit(sprite, [i*spriteSize*scale, i*spriteSize*scale]);
+	};
 };
 gamejs.preload(['sprites.png']);
 gamejs.ready(main);
