4059ef69a68c0e8d87fa475fb2ac6dbd7af0b1f6
Eduard Luhtonen <luhtonen@gmail.com>
2013-11-28 10:57:07 +0100

    adding hits, health and scoring

--- fighting/main.js
+++ fighting/main.js
@@ -6,38 +6,83 @@ var screenHeight = 600;
 var spriteSize = 128;
 var numSprites = 4;
 var up = 1, down = 2, left = 4, right = 8, canChange = 16;
+var forms = [];
+var timeBetweenHits = 300;
+var timeSinceHit = 0;
+var activeGame = true;
+var defaultFont = new font.Font("40px Arial");
 
-function Player(placement, form, forms) {
+function Player(placement, formIndex) {
 	this.placement = placement;
-	this.form = form;
-	this.forms = forms;
+	this.form = forms[formIndex];
 	this.mask = 16;
+	this.hit = false;
+	this.health = 30;
 };
-Player.prototype.changeForm = function(next_or_previous) {
-	this.form = this.forms[this.form[next_or_previous]];
+Player.prototype.changeForm = function(index) {
+	this.form = forms[index];
+};
+Player.registerHit = function(player1, player2) {
+	player1Index = player1.form.index;
+	player2Index = player2.form.index;
+	if (player1Index === 0) {
+		if (player2Index === 1) {
+			player1.hit = true;
+		} else if (player2Index === 2) {
+			player2.hit = true;
+		};
+	} else if (player1Index === 1) {
+		if (player2Index === 0) {
+			player2.hit = true;
+		} else if (player2Index === 2) {
+			player1.hit = true;
+		};
+	} else if (player1Index === 2) {
+		if (player2Index === 0) {
+			player1.hit = true;
+		} else if (player2Index === 1) {
+			player2.hit = true;
+		};
+	} else {
+		player1.hit = true;
+	}
+	if (player2Index === 3) {
+		player2.hit = true;
+	}
+	if (player2Index !== player1Index || player1Index === 3) {
+		timeSinceHit = 0;
+	};
 };
 Player.prototype.update = function(msDuration) {
 	if (this.mask & up) {
 		if (this.mask & canChange) {
-			this.changeForm('previous');
+			this.changeForm((this.form.index+3-1)%3);
 			this.mask &= ~canChange;
 		}
 	}
 	if (this.mask & down) {
 		if (this.mask & canChange) {
-			this.changeForm('next');
+			this.changeForm((this.form.index+1)%3);
 			this.mask &= ~canChange;
 		}
 	}
 	if (this.mask & left) {
-		this.placement = this.placement - 14;
+		if (this.placement > 0) {
+			this.placement = this.placement - 14;
+		}
 	}
 	if (this.mask & right) {
-		this.placement = this.placement + 14;
+		if (this.placement < 1000) {
+			this.placement = this.placement + 14;
+		}
+	}
+	if (this.hit === true) {
+		this.health = this.health - 3;
+		this.hit = false;
 	}
 };
 Player.prototype.draw = function(display) {
-	display.blit(this.form.image, [this.placement, 0]);
+	display.blit(this.form.image, [this.placement, 80]);
 };
 function main() {
 	var display = gamejs.display.setMode([screenWidth, screenHeight]);
@@ -53,28 +98,20 @@ function main() {
 		var maskCacheElement = mask.fromSurface(surface);
 		maskCache.push(maskCacheElement);
 	};
-	var forms = {
-		rock: 
-		{ image: surfaceCache[0],
-		  mask: maskCache[0],
-		  next: 'paper',
-		  previous: 'scissors' },
-		paper: 
-		{ image: surfaceCache[1], 
-		  mask: maskCache[1],
-		  next: 'scissors',
-		  previous: 'rock' },
-		scissors:
-		{ image: surfaceCache[2],
-		  mask: maskCache[2],
-		  next: 'rock',
-		  previous: 'paper'},
-		person:
-		{ image: surfaceCache[3],
-		  mask: maskCache[3],
-		  next: 'rock',
-		  previous: 'scissors'}
-	};
+	forms = [
+		{ index: 0,
+		  image: surfaceCache[0],
+		  mask: maskCache[0] },
+		{ index: 1,
+		  image: surfaceCache[1], 
+		  mask: maskCache[1] },
+		{ index: 2,
+		  image: surfaceCache[2],
+		  mask: maskCache[2] },
+		{ index: 3,
+		  image: surfaceCache[3],
+		  mask: maskCache[3] }
+	];
 	function handleEvent(event) {
 		if (event.type === gamejs.event.KEY_DOWN) {
 			if (event.key === gamejs.event.K_UP) {
@@ -123,24 +160,45 @@ function main() {
 		}
 	};
 	function gameTick(msDuration) {
-		gamejs.event.get().forEach(function(event) {
-			handleEvent(event);
-		});
-		display.clear();
-		var defaultFont = new font.Font("40px Arial");
-		var textSurface = defaultFont.render("ROCK PAPER SCISSORS", "#000000");
-		display.blit(textSurface, [0, 160]);
-		player1.update();
-		player2.update();
-		player1.draw(display);
-		player2.draw(display);
-		var hasMaskOverlap = player1.form.mask.overlap(player2.form.mask, [player1.placement - player2.placement, 0]);
-		if (hasMaskOverlap) {
-			console.log(hasMaskOverlap);
+		if(activeGame) {
+			gamejs.event.get().forEach(function(event) {
+				handleEvent(event);
+			});
+			display.clear();
+			if (timeSinceHit > timeBetweenHits) {
+				var hasMaskOverlap = player1.form.mask.overlap(player2.form.mask, [player1.placement - player2.placement, 0]);
+				if (hasMaskOverlap) {
+					Player.registerHit(player1, player2);
+				}
+			} else {
+				timeSinceHit += msDuration;
+			}
+			player1.update(msDuration);
+			player2.update(msDuration);
+			
+			display.blit(defaultFont.render("ROCK PAPER SCISSORS", "#000000"), [300, 0]);
+			display.blit(defaultFont.render("Player 1: ", "#000000"), [0, 240]);
+			display.blit(defaultFont.render(player1.health, "#000000"), [170, 240]);
+			display.blit(defaultFont.render("Controls: W A S D", "#000000"), [0, 280]);
+			display.blit(defaultFont.render("Player 2: ", "#000000"), [600, 240]);
+			display.blit(defaultFont.render(player2.health, "#000000"), [770, 240]);
+			display.blit(defaultFont.render("Controls: \u2191 \u2193 \u2190 \u2192", "#000000"), [600, 280]);
+			
+			player1.draw(display);
+			player2.draw(display);
+			if (player1.health === 0 || player2.health === 0) {
+				activeGame = false;
+				if (player1.health === 0) {
+					display.blit(defaultFont.render("Player 1 Defeated", "#000000"), [0, 320]);
+				} 
+				if (player2.health === 0) {
+					display.blit(defaultFont.render("Player 2 Defeated", "#000000"), [600, 320]);
+				} 
+			}
 		}
 	};
-	var player1 = new Player(0, forms['person'], forms);
-	var player2 = new Player(1000, forms['person'], forms);
+	var player1 = new Player(0, 3);
+	var player2 = new Player(1000, 3);
 	gamejs.time.fpsCallback(gameTick, this, 60);
 };
 gamejs.preload(['sprites_big.png']);
