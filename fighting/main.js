08e855fb37ee29235d62a84c6d5df58580e6d97a
Eduard Luhtonen <luhtonen@gmail.com>
2013-11-28 07:21:59 +0100

    adding movement

--- fighting/main.js
+++ fighting/main.js
@@ -1,4 +1,5 @@
 var gamejs = require('gamejs');
+var font = require('gamejs/font');
 var screenWidth = 1200;
 var screenHeight = 600;
 var scale = 8;
@@ -8,12 +9,19 @@ var numSprites = 4;
 function scaleUp(image) {
 	return gamejs.transform.scale(image, [spriteSize*scale, spriteSize*scale]);
 };
-function Player(placement, sprite) {
+function Player(placement, form, forms) {
 	this.placement = placement;
-	this.sprite = sprite;
+	this.form = form;
+	this.forms = forms;
+};
+Player.prototype.nextForm = function() {
+	this.form = this.forms[this.form["next"]];
+};
+Player.prototype.previousForm = function() {
+	this.form = this.forms[this.form["previous"]];
 };
 Player.prototype.draw = function(display) {
-	sprite = scaleUp(this.sprite);
+	sprite = scaleUp(this.form.image);
 	display.blit(sprite, [spriteSize + this.placement, spriteSize]);
 };
 function main() {
@@ -27,28 +35,42 @@ function main() {
 		surface.blit(sprites, imgSize, rect);
 		surfaceCache.push(surface);
 	};
-	var rock = surfaceCache[0];
-	var paper = surfaceCache[1];
-	var scissors = surfaceCache[2];
-	var person = surfaceCache[3];
+	var forms = {
+		rock: 
+		{ image: surfaceCache[0],
+		  next: 'paper',
+		  previous: 'scissors' },
+		paper: 
+		{ image: surfaceCache[1], 
+		  next: 'scissors',
+		  previous: 'rock' },
+		scissors:
+		{ image: surfaceCache[2],
+		  next: 'rock',
+		  previous: 'paper'},
+		person:
+		{ image: surfaceCache[3],
+		  next: 'rock',
+		  previous: 'scissors'}
+	};
 	function handleEvent(event) {
 		if (gamejs.event.KEY_DOWN) {
 			if (event.key === gamejs.event.K_UP) {
-				player2.sprite = person;
+				player2.previousForm();
 			} else if (event.key === gamejs.event.K_DOWN) {
-				player2.sprite = paper;
+				player2.nextForm();
 			} else if (event.key === gamejs.event.K_RIGHT) {
-				player2.sprite = scissors;
+				player2.placement = player2.placement + 25;
 			} else if (event.key === gamejs.event.K_LEFT) {
-				player2.sprite = rock;
+				player2.placement = player2.placement - 25;
 			} else if (event.key === gamejs.event.K_w) {
-				player1.sprite = person;
+				player1.previousForm();
 			} else if (event.key === gamejs.event.K_a) {
-				player1.sprite = rock;
+				player1.placement = player1.placement - 25;
 			} else if (event.key === gamejs.event.K_s) {
-				player1.sprite = paper;
+				player1.nextForm();
 			} else if (event.key === gamejs.event.K_d) {
-				player1.sprite = scissors;
+				player1.placement = player1.placement + 25;
 			}
 		}
 	};
@@ -57,11 +79,14 @@ function main() {
 			handleEvent(event);
 		});
 		display.clear();
+		var defaultFont = new font.Font("40px Arial");
+		var textSurface = defaultFont.render("ROCK PAPER SCISSORS", "#000000");
+		display.blit(textSurface, [0, 160]);
 		player1.draw(display);
 		player2.draw(display);
 	};
-	var player1 = new Player(0, person);
-	var player2 = new Player(200, person);
+	var player1 = new Player(0, forms['person'], forms);
+	var player2 = new Player(1000, forms['person'], forms);
 	gamejs.time.fpsCallback(gameTick, this, 60);
 };
 gamejs.preload(['sprites.png']);
