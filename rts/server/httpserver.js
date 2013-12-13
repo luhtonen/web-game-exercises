053683bd9a861dc5f18ffbfa3e421de5a9d873f3
Eduard Luhtonen <luhtonen@gmail.com>
2013-12-13 09:35:26 +0100

    real time with socket.io

--- rts/server/httpserver.js
+++ rts/server/httpserver.js
@@ -2,6 +2,7 @@ console.log("ahoy hoy, check me out at http://localhost:1234");
 var http = require('http');
 http.createServer(function(request, response) {
     console.log("Request received.");
+    response.writeHeader(200, {"Content-Type": "text/html"});
     response.write('<h1>AHOY HOY!</h1>');
     response.end();
 }).listen(1234);
